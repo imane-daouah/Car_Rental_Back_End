81eec3ae1381da60c9eb2cbdde37b08462fcad5f
imane-daouah <imane.daouah@gmail.com>
2024-11-21 23:42:41 +0100

    utilisateur deja un client

--- src/main/java/com/codewithprojects/spring/services/auth/AuthServiceImpl.java
+++ src/main/java/com/codewithprojects/spring/services/auth/AuthServiceImpl.java
@@ -5,39 +5,38 @@ import com.codewithprojects.spring.dto.UserDto;
 import com.codewithprojects.spring.entity.User;
 import com.codewithprojects.spring.enums.UserRole;
 import com.codewithprojects.spring.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
-public class AuthServiceImpl implements AuthService{
-    @Autowired
-    private  final UserRepository userReepository;
-    
+public class AuthServiceImpl implements AuthService {
+
+    private final UserRepository userRepository;
+
     @Override
     public UserDto createCustomer(SignupRequest signupRequest) {
-        User user=new User();
+        // Création d'un nouvel utilisateur
+        User user = new User();
         user.setNom(signupRequest.getNom());
         user.setEmail(signupRequest.getEmail());
         user.setPassword(signupRequest.getPassword());
         user.setPrenom(signupRequest.getPrenom());
         user.setAdresse(signupRequest.getAdresse());
-        user.setUserRole(UserRole.CUSTOMER);
         user.setNumero_tel(signupRequest.getNumero_tel());
         user.setUserRole(UserRole.CUSTOMER);
-        User creerUser = userReepository.save(user);
-        UserDto userDto=new UserDto();
+
+        // Sauvegarder l'utilisateur dans la base de données
+        User creerUser = userRepository.save(user);
+
+        // Créer un DTO pour retourner les données au client
+        UserDto userDto = new UserDto();
         userDto.setId(creerUser.getId());
         return userDto;
     }
+
     @Override
     public boolean hasCustomerWithEmail(String email) {
         return userRepository.findFirstByEmail(email).isPresent();
     }
-
-
-
 }
